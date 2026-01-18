9c0fa80e46989d81e45c5b870edbb5947c48c295
cagataycemaksoy <cemaksoy7890@gmail.com>
2026-01-18 16:19:24 +0100

    Initial Commit

---
+++ FruityApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FruityApp
+//
+//  Created by Cem Aksoy on 18.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruityApp/FruityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruityAppApp.swift
+//  FruityApp
+//
+//  Created by Cem Aksoy on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FruityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
