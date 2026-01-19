546184c2b16be057edaef419c620eff2c24bba4f
cagataycemaksoy <cemaksoy7890@gmail.com>
2026-01-19 13:11:25 +0100

    changed UI

--- FruityApp/ContentView.swift
+++ FruityApp/ContentView.swift
@@ -10,18 +10,41 @@ import SwiftUI
 struct ContentView: View {
   @State private var fruits = Fruits()
   
-    var body: some View {
-      NavigationStack {
-        List(fruits.fruits, id: \.self) { fruit in
-          Text(fruit.name)
-        }
-        .listStyle(.plain)
-        .navigationTitle("Fruits")
-        .task {
-          await fruits.getData()
+  var body: some View {
+    NavigationStack {
+      ScrollView {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 105), spacing: 12)], spacing: 10) {
+          ForEach(fruits.fruits, id: \.self) { fruit in
+            NavigationLink {
+              Text(fruit.name)
+            } label: {
+              ZStack{
+                RoundedRectangle(cornerRadius: 15)
+                  .fill(Color("background2"))
+                
+                Text(fruit.name)
+                  .font(.title3.bold())
+                  .foregroundStyle(.black)
+                  .frame(maxWidth: .infinity, maxHeight: .infinity)
+                  .overlay(alignment: .bottomTrailing) {
+                    Image(systemName: "chevron.right")
+                      .foregroundStyle(.black.opacity(0.2))
+                      .padding(6)
+                  }
+              }
+              .frame(minHeight: 130)
+            }
+            
+          }
         }
       }
+      .padding()
+      .navigationTitle("Fruits")
+      .task {
+        await fruits.getData()
+      }
     }
+  }
 }
 
 #Preview {
